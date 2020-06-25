36dcf9cd814cf2773bca65d906f90f195e06cc46
erik erik <erikerik771@gmail.com>
2020-06-25 20:23:51 +0300

    LEET

---
+++ XOROperationInAnArray/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace XOROperationInAnArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("n este:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("start este:");
+            int start = Convert.ToInt32(Console.ReadLine());
+            Solution solutie = new Solution();
+            int rezultat=solutie.XorOperation(n, start);
+            Console.WriteLine("rezultatul este: "+rezultat); 
+        }
+    }
+}
---
+++ XOROperationInAnArray/Solution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XOROperationInAnArray
+{
+    public class Solution
+    {
+        public int XorOperation(int n, int start)
+        {
+            int rezultat = 0;
+            for (int i = 0; i < n; i++)
+            {
+                rezultat = rezultat ^ (start + 2 * i);
+            }
+            return rezultat;
+        }
+    }
+}
