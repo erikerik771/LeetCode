a787a21722c37c1e87841f924d853ad4399787aa
erik erik <erikerik771@gmail.com>
2020-06-26 14:40:53 +0300

    >75 acceptance easy

---
+++ SubtractTheProductAndSumOfDigitsOfAnInteger/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SubtractTheProductAndSumOfDigitsOfAnInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti un numar:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Produsul cifrelor - suma cifrelor din n este:");
+            Console.WriteLine(Solution.SubtractProductAndSum(n));
+        }
+    }
+}
---
+++ SubtractTheProductAndSumOfDigitsOfAnInteger/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SubtractTheProductAndSumOfDigitsOfAnInteger
+{
+    public class Solution
+    {
+        public static int SubtractProductAndSum(int n)
+        {
+            int prod = 1;
+            int sum = 0;
+            int digit = n;
+            string numbers = n.ToString();
+            for(int i=0;i<numbers.Length;i++)
+            {
+                digit = int.Parse(numbers[i].ToString());
+                prod = prod * digit;
+                sum= sum+digit;
+            }
+            return prod - sum;
+        }
+    }
+}
