f3e4505490807af6ad105f64afea736a38a63d16
erik erik <erikerik771@gmail.com>
2020-06-26 15:52:48 +0300

    >75 acceptance easy

---
+++ CreateTargetArrayInTheGivenOrder/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CreateTargetArrayInTheGivenOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("cate numere aveti in array?");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] numere = new int[n];
+            int[] index = new int[n];
+            for(int i=0;i<n;i++)
+            {
+                Console.Write("Numarul " + (i+1) + ":");
+                numere[i] = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Indexul numarului " + (i+1) + ":");
+                index[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int[] rezultat=Solution.CreateTargetArray(numere, index);
+            Console.Write("Elementele sortate sunt: ");
+            foreach(int elem in rezultat)
+            {
+                Console.Write(elem + " ");
+            }
+        }
+    }
+}
---
+++ CreateTargetArrayInTheGivenOrder/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CreateTargetArrayInTheGivenOrder
+{
+    public class Solution
+    {
+        public static int[] CreateTargetArray(int[] nums, int[] index)
+        {
+            IList<int> target = new List<int>();
+            for(int i=0;i<nums.Length;i++)
+            {
+                target.Insert(index[i], nums[i]);
+            }
+            return target.ToArray();
+        }
+    }
+}
